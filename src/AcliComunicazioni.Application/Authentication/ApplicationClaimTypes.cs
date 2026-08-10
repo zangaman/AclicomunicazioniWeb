8c5bf9e65fe62585156b5dffe2e926aeec35e390
zangaman <81763109+zangaman@users.noreply.github.com>
2026-08-10 16:16:41 +0200

    Add application authentication claim types

---
+++ src/AcliComunicazioni.Application/Authentication/ApplicationClaimTypes.cs
@@ -0,0 +1,8 @@
+namespace AcliComunicazioni.Application.Authentication;
+
+public static class ApplicationClaimTypes
+{
+    public const string UserId = "application_user_id";
+    public const string DisplayName = "display_name";
+    public const string Role = "application_role";
+}
